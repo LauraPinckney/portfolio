a395a0c678bbaf307cad2f56bd6d3d97ea87a6e5
Laura Pinckney <laura.pinckney@yahoo.com>
2024-03-15 12:48:02 +0000

    update to json data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,29 +1,39 @@
 export const projects = [
   {
     id: 'capstone',
-    title: 'is this working',
-    about: 'please work',
-    tech: 'JavaScript',
+    title: 'How Sick is my Baby?',
+    about:
+      'As a first time mum on maternity leave I found myself going down the Google rabbit hole late at night when support services were shut. I found a useful PDF but it was not accessible for sleep deprived caregiver’s. This app is designed to be user friendly, to assist in the decision of what level of medical care their baby needs. Uses cumulative scores that link to the relevant health advice suggested for the baby.',
+    tech: ['JavaScript', 'HTML', 'CSS', 'React', 'GitHub'],
     liveCode: 'https://github.com/LauraPinckney/HowSickIsMyBaby-API',
     sourceCode: 'https://github.com/LauraPinckney/HowSickIsMyBaby-API',
     image: '',
   },
   {
     id: 'roboFriends',
-    title: 'is this working2',
-    about: 'please work2',
-    tech: 'JavaScript2',
-    liveCode: 'https://github.com/LauraPinckney/HowSickIsMyBaby-API',
-    sourceCode: 'https://github.com/LauraPinckney/HowSickIsMyBaby-API',
+    title: 'robo friends',
+    about: 'Developed as part of my React learning. Fun app that uses an API.',
+    tech: ['JavaScript', 'HTML', 'CSS', 'GitHub'],
+    liveCode: '',
+    sourceCode: 'https://github.com/LauraPinckney/robofriends',
     image: '',
   },
   {
-    id: 'background generator',
-    title: 'is this working3',
-    about: 'please work3',
-    tech: 'JavaScript3',
-    liveCode: 'https://github.com/LauraPinckney/HowSickIsMyBaby-API',
-    sourceCode: 'https://github.com/LauraPinckney/HowSickIsMyBaby-API',
+    id: 'backgroundGenerator',
+    title: 'Background Generator',
+    about: 'Developed as part of my JavaScript learning.',
+    tech: ['JavaScript', 'HTML', 'CSS', 'GitHub'],
+    liveCode: '',
+    sourceCode: 'https://github.com/LauraPinckney/BackgroundGenerator',
     image: '',
+  },
+  {
+    id: 'dateNightDecider',
+    title: 'Date Night Decider',
+    about:
+      'Application created during first hackathon of bootcamp to decide on what to do for date night. Random selector of movie, cocktail and trivia to go through whilst the movie is loaded and cocktail is made. Used various APIs and worked in a team of two.',
+    tech: ['Javascript', 'HTML', 'CSS'],
+    sourceCode:
+      'https://github.com/LauraPinckney/Team-8-The-Tech-Titans-plus-One/tree/main',
   },
 ];
